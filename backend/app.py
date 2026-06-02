d1fb8ee2274effb47eb35df9e4262b70385c88fd
DaniilAsk5 <daniilaskalin818@gmail.com>
2026-06-02 13:22:43 +0300

    feat: A project has been created, along with a simple HTTP application in python app.py and a Dockerfile that runs as a created user instead of root

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHandler(BaseHTTPRequestHandler):
+  def to_Get(self):
+    if self.path == "/":
+      self.send_response(200)
+      self.send_header('Content-type', 'text/plain; charset=utf-8')
+      self.end_headers()
+      self.wfile.write(b"Hello from Effective Mobile!")
+    else:
+      self.send_error(404, "Not_found")
+
+def run(server_class=HTTPServer, handler_class=SimpleHandler, port=8080):
+  server_address=('', port)
+  httpd = server_class(server_address, handler_class)
+  print(f'Server is starting on port {port}')
+  httpd.serve_forever()
+
+if __name__ == "__main__":
+  run()
+
